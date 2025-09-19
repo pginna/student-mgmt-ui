fe964175ed9aafa26b943ab788b94bf72797e0fb
JC <jrchiluka@gmail.com>
2025-09-18 21:10:16 -0400

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import {FormsModule} from '@angular/forms';
+import { UpdateStudentComponent } from './update-student/update-student.component';
+import { DeleteStudentComponent } from './delete-student/delete-student.component';
+import { StudentDetailsComponent } from './student-details/student-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentListComponent,
+    CreateStudentComponent,
+    UpdateStudentComponent,
+    DeleteStudentComponent,
+    StudentDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
